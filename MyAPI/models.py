078913ff0ea2ebf9804b62a6c6ed97a67f1fd64c
kimono-dell <alberto.gruning.zen@gmail.com>
2020-11-23 19:04:04 -0300

    migration ok

--- MyAPI/models.py
+++ MyAPI/models.py
@@ -3,24 +3,24 @@ from django.db.models.enums import Choices
 
 class aprovals(models.Model):
     GENDER_CHOICES = (
-        ('Female','Female')
+        ('Female','Female'),
         ('Male','Male')   
     )
     MARRIED_CHOICES = (
-        ('Yes','Yes')
+        ('Yes','Yes'),
         ('No','No')   
     )
     GRADUATED_CHOICES = (
-        ('Graduated','Graduated')
+        ('Graduated','Graduated'),
         ('Not_Graduated','Not_Graduated')   
     )
     SELFEMPLOYED_CHOICES = (
-        ('Yes','Yes')
+        ('Yes','Yes'),
         ('No','No')   
     )
     PROPERTY_CHOICES = (
-        ('Rural','Rural')
-        ('SemiUrban','SemiUrban')
+        ('Rural','Rural'),
+        ('SemiUrban','SemiUrban'),
         ('Urban','Urban') 
     )
 
@@ -33,7 +33,7 @@ class aprovals(models.Model):
     loantern = models.IntegerField()
     credithistory = models.IntegerField()
     gender = models.CharField(max_length=15, choices=GENDER_CHOICES)
-    married = models.CharField(max_length=15, Choices=MARRIED_CHOICES)
+    married = models.CharField(max_length=15, choices=MARRIED_CHOICES)
     graduatededucation = models.CharField(max_length=15, choices=GRADUATED_CHOICES)
     selfemployed = models.CharField(max_length=15, choices=SELFEMPLOYED_CHOICES)
     area = models.CharField(max_length=15, choices=PROPERTY_CHOICES)
